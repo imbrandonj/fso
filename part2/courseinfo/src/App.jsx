c0abc73541f2c5a7b4d52605e0a5c5db31437735
Brandon <branj.dev@gmail.com>
2023-05-28 18:08:16 -0500

    `part2` creation and file configuration

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,31 @@
+// imported components:
+import Header from './Header.jsx';
+import Content from './Content.jsx';
+
+/* course information exercise */
+export default function App() {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+}
